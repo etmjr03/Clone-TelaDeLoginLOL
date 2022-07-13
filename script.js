b5c0ab7b7c6ffcd0fec1af738f70b40a8b0c563e
etmjr03 <etmjr03@gmail.com>
2022-07-13 02:14:02 -0300

    Estilizando a tela de login do lol

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const handleFocus = ([ target ]) => {
     span.classList.add('span-active');
 }
 
+const handleFocusOut = ({ target })=> {
+
+    if(target.value == ' ') {
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+    
+}
+
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
